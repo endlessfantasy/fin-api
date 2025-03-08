70e48e3a4107ee24e1802ff910fd10f041154605
endlessfantasy <endless.fantasykawai@gmail.com>
2025-03-08 15:17:17 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from functions import get_stock_info, get_stock_data
+
+app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_methods=["*"], 
+    allow_headers=["*"], 
+)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/stock/{symbol}")
+def get_info(symbol: str):
+    return get_stock_info(symbol)
+
+@app.get("/stock-data/")
+def get_data(stock: str, days: int, interval: str = "1d"):
+    return get_stock_data(stock, days, interval)
+
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
